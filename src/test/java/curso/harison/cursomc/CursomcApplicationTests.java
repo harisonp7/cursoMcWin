78a02f8a2083d8b416c978c3a5ca495cb58c549f
Harison <harisonp@hotmail.com>
2018-07-04 23:08:42 -0300

    Projeto criado - Primeira versão do projetos

---
+++ src/test/java/curso/harison/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,16 @@
+package curso.harison.cursomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursomcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

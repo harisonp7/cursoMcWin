2c81077113408ab20a67e70174b1fdf75a7ffbca
Harison <harisonp@hotmail.com>
2018-07-05 22:18:11 -0300

    Rest testado

---
+++ src/main/java/curso/harison/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,17 @@
+package curso.harison.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResources {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Esse Rest esta funcionando";
+
+	}
+
+}
